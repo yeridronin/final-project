97050d7aeaed52946420937d76026b0936a18f00
yeridronin <yeridronin@gmail.com>
2025-03-11 09:38:20 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
