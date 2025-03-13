d238e7a19ee12b7ae783c66c1030845fecb19aa6
yeridronin <yeridronin@gmail.com>
2025-03-13 11:12:39 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe(() => {
+        this.router.navigate(['/dashboard']);
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+}
