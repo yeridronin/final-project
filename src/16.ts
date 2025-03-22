066b5d7b423603e89ffe3aacb270ed80a51bb193
yeridronin <yeridronin@gmail.com>
2025-03-22 16:59:19 +0000

    Add 16.ts

---
+++ src/16.ts
@@ -0,0 +1,10 @@
+let x = 5;
+let y = 10;
+
+if (x > y) {
+    console.log("x is greater than y");
+} else if (x < y) {
+    console.log("x is less than y");
+} else {
+    console.log("x and y are equal");
+}
