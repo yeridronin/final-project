722a0174866816101bcaea9b444c6397481ae6b9
yeridronin <yeridronin@gmail.com>
2025-05-27 23:54:59 +0000

    Add 48.ts

---
+++ src/48.ts
@@ -0,0 +1,9 @@
+function main() {
+  const input = "Hello, world!";
+  
+  // Process and manipulate the input string
+  let processed = input.replace(/ /g, "");
+  console.log(processed);
+}
+
+main();
